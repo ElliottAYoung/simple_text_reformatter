9be46762b01e8c4a5af2317fcd49acf0bcd6ed39
Elliott Young <elliott.a.young@gmail.com>
2020-08-25 10:44:52 -0500

    feat: Initial Commit

---
+++ lib/simple_text_reformatter.rb
@@ -0,0 +1,6 @@
+require "simple_text_reformatter/version"
+
+module SimpleTextReformatter
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/simple_text_reformatter/version.rb
@@ -0,0 +1,3 @@
+module SimpleTextReformatter
+  VERSION = "0.1.0"
+end
